3fa8d25a2cfeb4b30f55442677070978a6c4136d
AndreTInfante <aticper@gmail.com>
2015-01-24 02:27:51 -0700

    Got basic animation tools working
    Animation controller is... in progress

---
+++ Dinner/Assets/AnimController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour {
+	public GameObject[] targets;
+	public int[] type;
+	public bool active;
+	private int counter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Trigger(){
+		active = !active;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		counter = 0;
+		if(active){
+			foreach (GameObject target in targets){
+				if(type[counter] == 0){
+					//target.
+				}
+				counter++;
+			}
+		}
+	}
+}
---
+++ Dinner/Assets/PropRotateLock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropRotateLock : MonoBehaviour {
+	public bool active;
+	public Vector3 offset;
+	public float speed;
+	private Quaternion temp;
+	private Quaternion target;
+
+	// Use this for initialization
+	void Start () {
+		target = Quaternion.Euler(transform.localRotation.eulerAngles + offset);
+	}
+	
+	void LockTrigger(){
+		active = !active;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(active){
+			transform.localRotation = Quaternion.Lerp(transform.localRotation, target, speed);
+		}
+	}
+}
