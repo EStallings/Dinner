a8d228a79026beb0920cab1b7d67b423049033df
AndreTInfante <aticper@gmail.com>
2015-01-24 15:22:49 -0700

    fine-tuning nodding

--- Dinner/Assets/NodListener.cs
+++ Dinner/Assets/NodListener.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NodListener : MonoBehaviour {
 	public int buffersize;
+	public bool nodding;
 	public Vector3[] orientations;
 	public Vector3[] derivatives;
 	public Vector3[] smoothderivatives;
@@ -15,6 +16,7 @@ public class NodListener : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		nodding = false;
 		numhighlights = 20;
 		nodtest = 0;
 		orientations = new Vector3[buffersize];
@@ -41,22 +43,27 @@ public class NodListener : MonoBehaviour {
 		//collect high data points into nod test array 
 		if(index >= 5){
 			highpoints[Mathf.RoundToInt(Time.timeSinceLevelLoad*numhighlights*2)%numhighlights] = 0.0f;
-			if(Mathf.Abs(smoothderivatives[index-5].x) > 1.5f){
-				highpoints[Mathf.RoundToInt(Time.timeSinceLevelLoad*numhighlights*2)%numhighlights] = smoothderivatives[index-5].x;
+			if(Mathf.Abs(smoothderivatives[index-5].x) > 0.1f){
+				if(Mathf.Abs(smoothderivatives[index-5].x) > Mathf.Abs(smoothderivatives[index-5].y) && (Mathf.Abs(smoothderivatives[index-5].x) > Mathf.Abs(smoothderivatives[index-5].z))){
+					highpoints[Mathf.RoundToInt(Time.timeSinceLevelLoad*numhighlights*2)%numhighlights] = smoothderivatives[index-5].x;
+				}
 			}
+
 		}
 		nodtest = 0;
 		for(i = 1; i < numhighlights-1; i++){
-			if(highpoints[i-1] != 0 && highpoints[i] !=0){
+			if(highpoints[i-1] != 0 && highpoints[i] !=0.5){
 				if (Mathf.Sign(highpoints[i-1]) != Mathf.Sign(highpoints[i])){
 					nodtest++;
 				}
 			}
 		}
-		if (nodtest >= 4){
-			print ("You nodded!");
+		if (nodtest >= 2){
+			nodding = true;
+			//print ("You nodded!");
 		}else{
-			print ("|");
+			nodding = false;
+			//print ("|");
 		}
 	
 
